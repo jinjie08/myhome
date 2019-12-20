8871a64065dc6052b75a955e9a38a1d57cfdc5f2
jinjie <jinj114@chinaunicom.cn>
2019-12-20 16:19:29 +0800

    ts-lint规则修改

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,10 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background: black;
+}
+body {
+  margin: 0;
 }
 </style>
